273ee44c94d1de64305b06408d835ec1816bfe4c
frans-arya <63969536+frans-arya@users.noreply.github.com>
2020-05-11 13:47:43 +0700

    Staff's Solution

---
+++ village-networks.py
@@ -0,0 +1,122 @@
+from collections import Counter
+import numpy as np
+
+#we will calculate the chance homophily for an arbitrary characteristic. 
+#Homophily is the proportion of edges in the network whose constituent 
+#nodes share that characteristic.
+
+#How much homophily do we expect by chance? If characteristics are 
+#distributed completely randomly, the probability that two nodes  
+#x  and  y  share characteristic  a  is the probability both nodes 
+#have characteristic  a  , which is the marginal probability of  
+#a  squared. The total probability that nodes  x  and  y  share 
+#their characteristic is therefore the sum of the square of the marginal 
+#probabilities of each characteristic in the network.
+
+#Create a function marginal_prob that takes a dictionary chars with 
+#personal IDs as keys and characteristics as values; it should return 
+#a dictionary with characteristics as keys and their marginal probability 
+#(frequency of occurence of a characteristic divided by the sum of 
+#frequencies of each characteristic) as values.
+#Create a function chance_homophily(chars) that takes a dictionary chars 
+#defined as above and computes the chance homophily (homophily due to 
+#chance alone) for that characteristic.
+
+#A sample of three peoples' favorite colors is given in favorite_colors. 
+#Use your function to compute the chance homophily in this group, and 
+#store it as color_homophily.
+
+#Print color_homophily.
+
+def marginal_prob(chars):
+    frequencies = dict(Counter(chars.values()))
+    sum_frequencies = sum(frequencies.values())
+    return {char: freq / sum_frequencies for char, freq in frequencies.items()}
+                
+def chance_homophily(chars):
+    marginal_probs = marginal_prob(chars)
+    return np.sum(np.square(list(marginal_probs.values())))
+
+favorite_colors = {
+    "ankit":  "red",
+    "xiaoyu": "blue",
+    "mary":   "blue"
+}
+
+color_homophily = chance_homophily(favorite_colors)
+print(color_homophily)
+
+import pandas as pd
+
+df  = pd.read_csv("https://courses.edx.org/asset-v1:HarvardX+PH526x+2T2019+type@asset+block@individual_characteristics.csv", low_memory=False, index_col=0)
+df1 = df[df.village == 1]
+df2 = df[df.village == 2]
+
+#we want to define a few dictionaries that enable us 
+#to look up the sex, caste, and religion of members 
+#of each village by personal ID. For Villages 1 and 2, 
+#their personal IDs are stored as pid.
+
+sex1 = df1.set_index("pid")["resp_gend"].to_dict()
+caste1 = df1.set_index("pid")["caste"].to_dict()
+religion1 = df1.set_index("pid")["religion"].to_dict()
+
+sex2 = df2.set_index("pid")["resp_gend"].to_dict()
+caste2 = df2.set_index("pid")["caste"].to_dict()
+religion2 = df2.set_index("pid")["religion"].to_dict()
+
+print("Village 1 chance of same sex:", chance_homophily(sex1))
+print("Village 1 chance of same caste:", chance_homophily(caste1))
+print("Village 1 chance of same religion:", chance_homophily(religion1))
+
+print("Village 2 chance of same sex:", chance_homophily(sex2))
+print("Village 2 chance of same caste:", chance_homophily(caste2))
+print("Village 2 chance of same religion:", chance_homophily(religion2))
+
+
+def homophily(G, chars, IDs):
+    """
+    Given a network G, a dict of characteristics chars for node IDs,
+    and dict of node IDs for each node in the network,
+    find the homophily of the network.
+    """
+    num_same_ties = 0
+    num_ties = 0
+    for n1, n2 in G.edges():
+        if IDs[n1] in chars and IDs[n2] in chars:
+            if G.has_edge(n1, n2):
+                num_ties += 1
+                if chars[IDs[n1]] == chars[IDs[n2]]:
+                    num_same_ties += 1
+    return (num_same_ties / num_ties)
+  
+data_filepath1 = "https://courses.edx.org/asset-v1:HarvardX+PH526x+2T2019+type@asset+block@key_vilno_1.csv"
+data_filepath2 = "https://courses.edx.org/asset-v1:HarvardX+PH526x+2T2019+type@asset+block@key_vilno_2.csv"
+
+pid1 = pd.read_csv(data_filepath1, index_col = 0)
+pid2 = pd.read_csv(data_filepath2, index_col = 0)
+
+import networkx as nx
+A1 = np.array(pd.read_csv("https://courses.edx.org/asset-v1:HarvardX+PH526x+2T2019+type@asset+block@adj_allVillageRelationships_vilno1.csv", index_col=0))
+A2 = np.array(pd.read_csv("https://courses.edx.org/asset-v1:HarvardX+PH526x+2T2019+type@asset+block@adj_allVillageRelationships_vilno2.csv", index_col=0))
+G1 = nx.to_networkx_graph(A1)
+G2 = nx.to_networkx_graph(A2)
+
+pid1 = pd.read_csv(data_filepath1, dtype=int)['0'].to_dict()
+pid2 = pd.read_csv(data_filepath2, dtype=int)['0'].to_dict()
+
+print("Village 1 observed proportion of same sex:", homophily(G1, sex1, pid1))
+print("Village 1 observed proportion of same caste:", homophily(G1, caste1, pid1))
+print("Village 1 observed proportion of same religion:", homophily(G1, religion1, pid1))
+
+print("Village 2 observed proportion of same sex:", homophily(G2, sex2, pid2))
+print("Village 2 observed proportion of same caste:", homophily(G2, caste2, pid2))
+print("Village 2 observed proportion of same religion:", homophily(G2, religion2, pid2))
+
+print("Village 1 chance of same sex:", chance_homophily(sex1))
+print("Village 1 chance of same caste:", chance_homophily(caste1))
+print("Village 1 chance of same religion:", chance_homophily(religion1))
+
+print("Village 2 chance of same sex:", chance_homophily(sex2))
+print("Village 2 chance of same caste:", chance_homophily(caste2))
+print("Village 2 chance of same religion:", chance_homophily(religion2))
