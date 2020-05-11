47596f8ddd957590cbcf78095c41c5379a8823a9
frans-arya <63969536+frans-arya@users.noreply.github.com>
2020-05-11 17:36:43 +0700

    Create log-classification-example.py

---
+++ linear-regression/log-classification-example.py
@@ -0,0 +1,17 @@
+def plot_probs(ax, clf, class_no):
+    xx1, xx2 = np.meshgrid(np.arange(-5, 5, 0.1), np.arange(-5, 5, 0.1))
+    probs = clf.predict_proba(np.stack((xx1.ravel(), xx2.ravel()), axis=1))
+    Z = probs[:,class_no]
+    Z = Z.reshape(xx1.shape)
+    CS = ax.contourf(xx1, xx2, Z)
+    cbar = plt.colorbar(CS)
+    plt.xlabel("$X_1$")
+    plt.ylabel("$X_2$")
+
+plt.figure(figsize=(5,8))
+ax = plt.subplot(211)
+plot_probs(ax, clf, 0)
+plt.title("Pred. prob for class 1")
+ax = plt.subplot(212)
+plot_probs(ax, clf, 1)
+plt.title("Pred. prob for class 2");
